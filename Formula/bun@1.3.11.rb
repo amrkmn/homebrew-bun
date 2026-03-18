1add56ab735d8b9b74e05630debaaef0e8b0ed52
BrewTestBot <brew-test-bot@users.noreply.github.com>
2026-03-18 22:36:58 +0800

    fix: add missing bottle block and fix extra blank line

--- Formula/bun@1.3.11.rb
+++ Formula/bun@1.3.11.rb
@@ -5,6 +5,10 @@ class BunAT1311 < Formula
   sha256 "83cb4f74272d21fdfc001b1c0cb9ea0d6a07af741409d49153a31687e03c01e8"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/bun"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "abd610c0e5efe5a9a87f7a1ede7393cb150d71804f6df908e2819955d2011424"
+  end
 
   depends_on "node" => :build
 
